cb4a244f851fd2915ce51ef7ffcff9b5072127e0
samayoapp <42884731+samayoapp@users.noreply.github.com>
2019-03-12 23:54:05 -0600

    He creado el Index de mi sitio
    Pienso trabajarlo en PHP desde el comienzo

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Allan Samayoa</title>
+</head>
+<body>
+	<h3>Bienvenido a:</h3>
+	<h1>AllanSamayoa.com</h1>
+	<h2>El sitio oficial de Allan Samayoa</h2>
+	<ul>
+		<li>Inicio</li>
+		<li>Sobre mí</li>
+		<li>Habilidades</li>
+		<li>Contacto</li>
+	</ul>
+</body>
+</html>
